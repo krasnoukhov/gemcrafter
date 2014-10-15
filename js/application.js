c327923e2cef84ac2fe89d6ce409306ea64005f5
Dmitry Krasnoukhov <dmitry@krasnoukhov.com>
2014-10-15 20:23:08 +0300

    Initial commit: basic parallax

---
+++ js/application.js
@@ -0,0 +1,31 @@
+var limit = 315;
+var offsets = {
+  "head": 80,
+  "fog": 210
+};
+
+if (!window.requestAnimationFrame) {
+  window.requestAnimationFrame = function(fn) { fn() }
+}
+
+var parallax = function(offset) {
+  for(var layer in offsets) {
+    var top = Math.round(offsets[layer]*(offset/limit));
+    frame(layer, offset, top)
+  }
+}
+
+var frame = function(layer, offset, top) {
+  requestAnimationFrame(function() {
+    $(".bg-"+layer).css("top", offset-top+"px");
+  });
+}
+
+$(window).on("scroll", function() {
+  var offset = window.scrollY;
+  if(offset <= limit) {
+    parallax(offset);
+  }
+})
+
+parallax(Math.min(window.scrollY,limit));
