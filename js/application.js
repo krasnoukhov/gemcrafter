990e149bebf96163041045956949b6328b37ef6b
Dmitry Krasnoukhov <dmitry@krasnoukhov.com>
2014-10-18 20:54:54 +0300

    Improve code a bit

--- js/application.js
+++ js/application.js
@@ -6,26 +6,26 @@ var offsets = {
 
 if (!window.requestAnimationFrame) {
   window.requestAnimationFrame = function(fn) { fn() }
-}
+};
 
 var parallax = function(offset) {
   for(var layer in offsets) {
     var top = Math.round(offsets[layer]*(offset/limit));
     frame(layer, offset, top)
   }
-}
+};
 
 var frame = function(layer, offset, top) {
   requestAnimationFrame(function() {
-    $(".bg-"+layer).css("top", offset-top+"px");
+    $(".bg-"+layer).css("top", (offset-top)+"px");
   });
-}
+};
 
 $(window).on("scroll", function() {
   var offset = window.scrollY;
   if(offset <= limit) {
     parallax(offset);
   }
-})
+});
 
-parallax(Math.min(window.scrollY,limit));
+parallax(Math.min(window.scrollY, limit));
